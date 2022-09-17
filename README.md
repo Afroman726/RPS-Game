64622ddc40d181a8dfa2b831a93ea73289b68da6
Afroman726 <dymitrihawkins@gmail.com>
2022-09-17 10:26:40 -0700

    Updated Js files.  Game fully works but score isn't working...yet /help

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# JavaScript
-ARRAYS
+ROCK,PAPER,SCISSORS:
+
+This program is my first attempt in making a fully functioning rock, paper, scissors game running off of JavaScript and DOM. 
--- practice.js
+++ practice.js
@@ -1,11 +1,15 @@
 
-/*const R = 'Rock';
+const R = 'Rock';
 const P = 'Paper';
 const S = 'Scissors';
 let playerScore = 0;
 let computerScore = 0;
-let computerSelection = getRandomChoice();
-let playerSelection = prompt("Choose Rock, Paper, or Scissors");
+
+
+
+
+
+
 
 function getRandomChoice(){             //This is the logic for the computer picking Rock Paper or Scissors
     let randomNumber = Math.floor(Math.random() * 3)
@@ -21,43 +25,45 @@ function getRandomChoice(){             //This is the logic for the computer pic
 
 
 
-}  
+};  
+
+    const game =()=> {
+    let rounds= prompt('How many rounds would you like to play?');
+        for (let i = 0; i < rounds; i++){
+
+         let playerSelection = prompt("Choose Rock, Paper, or Scissors");
+        
+         let computerSelection = getRandomChoice();
+        let score = 0;
+    
+    
+            if (playerSelection == computerSelection){
+            console.log("Tie!!")
+           }else  if (
+                (playerSelection === 'Rock' && computerSelection === 'Scissors') ||     //The actual execution for the game, Tie, Win, or Lose.
+                (playerSelection === 'Scissors' && computerSelection === 'Paper') ||
+                (playerSelection === 'Paper' && computerSelection === 'Rock')
+        ){ score++;
+            console.log("winner!  Your current score is " +score + '.' );
+           }else if (
+            (computerSelection === 'Rock' && playerSelection === 'Scissors') ||
+            (computerSelection === 'Scissors' && playerSelection === 'Paper') ||
+            (computerSelection === 'Paper' && playerSelection === 'Rock')
+        ) { score--;
+            console.log("LOSER! Your current score is " +score +' .');
+          
+             }
+        }
+    };
 
 
-function game(){
-
-for (let i = prompt("How many rounds would you like to do?")        //This spot allows user to pick the rounds
-; i < 10; i++){
-
-
-
-    function playRound(playerSelection, computerSelection){
-   if (playerSelection == computerSelection){
-    console.log("Tie!!")
-   }
-   else  if (
-        (playerSelection === 'Rock' && computerSelection === 'Scissors') ||     //The actual execution for the game, Tie, Win, or Lose.
-        (playerSelection === 'Scissors' && computerSelection === 'Paper') ||
-        (playerSelection === 'Paper' && computerSelection === 'Rock')
-){console.log("Winner!");
-} else if (
-    (computerSelection === 'Rock' && playerSelection === 'Scissors') ||
-    (computerSelection === 'Scissors' && playerSelection === 'Paper') ||
-    (computerSelection === 'Paper' && playerSelection === 'Rock')
-) { console.log("LOSER");
-
-}
-
-
-}
-}
-
-}
-
-
-
-
-
-
+   
 
 game()
+
+
+
+
+
+
+
